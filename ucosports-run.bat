84b3f74b9f32a29d98c55700066ded276eab9208
Crisman Alejandro Carmona Marulanda <crisman.carmona@techandsolve.com>
2017-03-29 13:01:03 -0500

    Ports Deploy

--- ucosports-run.bat
+++ ucosports-run.bat
@@ -1,3 +1,3 @@
 @echo off
-start "UCOSPORTS-TOURNAMENT-MS" java -jar %cd%/ucosports-tournament-ms/target/deploy-swarm.jar -Dswarm.port.offset=2
-start "UCOSPORTS-WEB" java -jar %cd%/ucosports-web/target/deploy-swarm.jar -Dswarm.port.offset=1
+start "UCOSPORTS-TOURNAMENT-MS" java -jar %cd%/ucosports-tournament-ms/target/deploy-swarm.jar
+start "UCOSPORTS-WEB" java -jar %cd%/ucosports-web/target/deploy-swarm.jar
