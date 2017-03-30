35a3626d42c27a198e44e483eebf3e872c1ee8eb
Crisman Carmona <crisman999@outlook.com>
2017-03-29 20:01:38 -0500

    Corrección de Nombres y creación scripts db

--- ucosports-tournament-domain/src/main/java/co/com/meerkats/ucosports/application/DependencyConfig.java
+++ ucosports-tournament-domain/src/main/java/co/com/meerkats/ucosports/application/DependencyConfig.java
@@ -3,7 +3,6 @@ package co.com.meerkats.ucosports.application;
 import javax.enterprise.context.Dependent;
 import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManager;
-import javax.persistence.FlushModeType;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceContextType;
 
@@ -15,11 +14,6 @@ public class DependencyConfig {
 	@Produces
 	@Dependent
 	public EntityManager getEntityManager() {
-		entityManager.setFlushMode(FlushModeType.COMMIT);
-		entityManager.getProperties().forEach((x,y) -> {
-			System.out.println(x + " " + y);
-		});
-		System.out.println("Entity manager");
 		return entityManager;
 	}
 	
