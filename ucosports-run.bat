f85666a168b91ba2ad32ea9f0b871a27724739d2
Crisman Alejandro Carmona Marulanda <crisman.carmona@techandsolve.com>
2017-05-02 15:56:10 -0500

    Posibilidad de escoger ambiente al desplegar

--- ucosports-run.bat
+++ ucosports-run.bat
@@ -1,3 +1,3 @@
 @echo off
-start "UCOSPORTS-TOURNAMENT-MS" java -jar %cd%/ucosports-tournament-ms/target/deploy-swarm.jar
+start "UCOSPORTS-TOURNAMENT-MS" java -jar %cd%/ucosports-tournament-ms/target/deploy-swarm.jar --profile=default
 start "UCOSPORTS-WEB" java -jar %cd%/ucosports-web/target/deploy-swarm.jar
