354adb234688ddf83e174d7663b5cc0865accd1a
Sebastian Eusse <s-e-b-a-s.94@hotmail.com>
2017-06-25 15:02:22 -0500

    Auto stash before merge of "ht" and "origin/ht"

--- hotelturin-ms/src/main/java/co/com/meerkats/hotelturin/logical/IHabitacionLogical.java
+++ hotelturin-ms/src/main/java/co/com/meerkats/hotelturin/logical/IHabitacionLogical.java
@@ -9,6 +9,11 @@ import co.com.meerkats.hotelturin.dto.HabitacionDTO;
 public interface IHabitacionLogical extends ILogicalCommon<Habitacion,HabitacionDTO> {
 	
 	List<HabitacionDTO> getAll();
+<<<<<<< Updated upstream
 	HabitacionDTO getbyEstate(EstadoDTO estado);
+=======
+	HabitacionDTO getbyEstate(Integer estado_id);
+	HabitacionDTO add(HabitacionDTO habitacion);
+>>>>>>> Stashed changes
 
 }
--- hotelturin-ms/src/main/java/co/com/meerkats/hotelturin/rest/HabitacionServiceRest.java
+++ hotelturin-ms/src/main/java/co/com/meerkats/hotelturin/rest/HabitacionServiceRest.java
@@ -10,7 +10,11 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+<<<<<<< Updated upstream
 import co.com.meerkats.hotelturin.dto.EstadoDTO;
+=======
+import co.com.meerkats.hotelturin.dto.ClienteDTO;
+>>>>>>> Stashed changes
 import co.com.meerkats.hotelturin.dto.HabitacionDTO;
 import co.com.meerkats.hotelturin.logical.IHabitacionLogical;
 
@@ -30,12 +34,22 @@ public class HabitacionServiceRest {
 	}
 
 	@POST
+	@Path("/add")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public HabitacionDTO add(HabitacionDTO habitacion) throws Exception{
+		return logical.add(habitacion);
+		
+	}	
+	@POST
 	@Path("/getByState")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	public HabitacionDTO getbyState (EstadoDTO estado){
 			return logical.getbyEstate(estado);
 	}
+	
+	
 	
 	
 	
